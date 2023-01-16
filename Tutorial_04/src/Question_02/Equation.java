b1d930e041fb3df59ce887b0916c32297717fe34
Pasindu_Marambe <pasindurmarambe00@gmail.com>
2023-01-16 10:03:10 +0530

    Add files via upload

---
+++ Tutorial_04/src/Question_02/Equation.java
@@ -0,0 +1,9 @@
+package Question_02;
+
+interface Equation {
+    public int numberOfVariables = 1;
+
+    public abstract double solve();
+
+    public void printFormula();
+}
---
+++ Tutorial_04/src/Question_02/LinearEquation.java
@@ -0,0 +1,17 @@
+package Question_02;
+
+public class LinearEquation implements Equation{
+
+    public void printFormula() {
+        System.out.println("x^2 + 3x + 1");
+    }
+
+    public double solve() {
+        //numberOfVariables = 2;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
